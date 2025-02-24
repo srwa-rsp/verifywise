567b5057afe140958e3dc1f70e137f77d7c3b0a9
srwa-rsp <srwa.rashidpoor.1993@gmail.com>
2025-02-24 11:58:57 -0500

    fix: handle risk modal

--- Clients/src/presentation/pages/Vendors/index.tsx
+++ Clients/src/presentation/pages/Vendors/index.tsx
@@ -18,10 +18,12 @@ import CustomStep from "../../components/PageTour/CustomStep";
 import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
+import AddNewRisk from "../../components/Modals/NewRisk";
 
 const Vendors = () => {
   const theme = useTheme();
   const [isOpen, setIsOpen] = useState(false);
+  const [isRiskModalOpen, setIsRiskModalOpen] = useState(false);
   const [value, setValue] = useState("1");
   const [vendorChangeTrigger, setVendorChangeTrigger] = useState(0);
   const [vendorRiskChangeTrigger, setVendorRiskChangeTrigger] = useState(0);
@@ -44,6 +46,9 @@ const Vendors = () => {
 
   const openAddNewVendor = () => {
     setIsOpen(true);
+  };
+  const openAddNewRisk = () => {
+    setIsRiskModalOpen(true);
   };
 
   const handleChange = (_: React.SyntheticEvent, newValue: string) => {
@@ -301,7 +306,7 @@ const Vendors = () => {
                   },
                 }}
                 onClick={() => {
-                  openAddNewVendor();
+                  openAddNewRisk();
                 }}
               >
                 Add new Risk
@@ -330,6 +335,13 @@ const Vendors = () => {
         setIsOpen={() => setIsOpen(false)}
         value={value}
         onVendorChange={updateVendorChangeTrigger}
+      />
+      <AddNewRisk
+        isOpen={isRiskModalOpen}
+        handleChange={handleChange}
+        setIsOpen={() => setIsRiskModalOpen(false)}
+        value={value}
+        onRiskChange={updateVendorRiskChangeTrigger}
       />
     </div>
   );
